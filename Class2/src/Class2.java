32657105ab514c1c4ae92e4968d97be676f35327
leh23 <leh23@LAPTOP-LV5PCV6K>
2019-11-19 17:46:32 +0900

    hotfix commit

--- Class2/src/Class2.java
+++ Class2/src/Class2.java
@@ -4,6 +4,7 @@ public class Class2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("원광대학교 컴퓨터소프트웨어공학과");
+		System.out.println("2학년");
 	}
 
 }
